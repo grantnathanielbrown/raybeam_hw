1241030e4e64146b1437141ef3f09ab1d90dd584
Grant Brown <grant@Grants-MacBook-Pro.local>
2022-09-29 00:50:52 -0400

    more CSS.

--- src/components/PlayerVsPlayer.tsx
+++ src/components/PlayerVsPlayer.tsx
@@ -3,7 +3,7 @@ import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContai
 import convertedAverages from '../data/convertedAverages.json';
 
 export default function PlayerVsPlayer() {
-    const barColors: string[] = ['red', 'blue', 'yellow', 'green', 'orange'];
+    const barColors: string[] = ['red', 'blue', 'purple', 'green', 'orange'];
     const [dataSubjects, setDataSubjects] = React.useState<string[]>([]);
     useEffect( () => {
         // Collect the names of all the players and the team, and store them in dataSubjects.
@@ -14,7 +14,9 @@ export default function PlayerVsPlayer() {
 
   return (
 
-    <ResponsiveContainer width="50%" height="50%">
+    <div className="pvp-container">
+      <h2>2017 Batting Averages</h2>
+      <ResponsiveContainer width="100%" height="85%">
         <LineChart
           width={500}
           height={300}
@@ -32,9 +34,10 @@ export default function PlayerVsPlayer() {
           <Tooltip />
           <Legend />
             {dataSubjects.map( (subject, index) => {
-                return <Line type="monotone" dataKey={subject} stroke={barColors[index]} strokeWidth={3} activeDot={{ r: 8 }} />
+                return <Line type="monotone" key={subject} dataKey={subject} stroke={barColors[index]} strokeWidth={3} activeDot={{ r: 8 }} />
             })}
         </LineChart>
       </ResponsiveContainer>
+    </div>
   )
 }
--- src/components/PlayerVsTeam.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function PlayerVsTeam() {
-  return (
-    <div>PlayerVsTeam</div>
-  )
-}
